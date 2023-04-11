023dc9d159b8fb82e9bc3746f3ab325726383dc0
rigoberto.gf <rigobertogomez.gf@gmail.com>
2023-04-10 20:09:50 -0600

    feat: :sparkles: Add delete task confirmation dialog

--- src/context/TasksContext.tsx
+++ src/context/TasksContext.tsx
@@ -1,14 +1,46 @@
 import { Task } from "@/models";
-import { useState, createContext, ReactNode, useContext } from "react";
+import {
+  useState,
+  useEffect,
+  createContext,
+  ReactNode,
+  useContext,
+} from "react";
 
-const TasksContext = createContext();
+enum TasksView {
+  TABLE = "TABLE",
+  GRID = "GRID",
+}
+
+interface TasksContextValue {
+  currentTask: Task | null;
+  setCurrentTask: React.Dispatch<React.SetStateAction<Task | null>>;
+  view: TasksView;
+  setView: (React.Dispatch<React.SetStateAction<TasksView>> | ((task: TasksView) => void));
+  openDeleteTaskDialog: boolean;
+  setOpenDeleteTaskDialog: React.Dispatch<React.SetStateAction<boolean>>;
+  openCreateTaskDialog: boolean;
+  setOpenCreateTaskDialog: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const TasksContext = createContext<TasksContextValue | null>(null);
+
+
+const localStorageView = localStorage.getItem("view");
 
 export const TasksContextProvider = ({ children }: { children: ReactNode }) => {
   const [currentTask, setCurrentTask] = useState<Task | null>(null);
+  const [view, setView] = useState<TasksView>(TasksView[localStorageView as keyof typeof TasksView]);
   const [openDeleteTaskDialog, setOpenDeleteTaskDialog] =
     useState<boolean>(false);
   const [openCreateTaskDialog, setOpenCreateTaskDialog] =
     useState<boolean>(false);
+
+
+  const handleView = (value: TasksView) => {
+    setView(value);
+    window.localStorage.setItem("view", value);
+  };
 
   return (
     <TasksContext.Provider
@@ -18,7 +50,9 @@ export const TasksContextProvider = ({ children }: { children: ReactNode }) => {
         openDeleteTaskDialog,
         setOpenDeleteTaskDialog,
         openCreateTaskDialog,
-        setOpenCreateTaskDialog
+        setOpenCreateTaskDialog,
+        view,
+        setView: handleView,
       }}
     >
       {children}
